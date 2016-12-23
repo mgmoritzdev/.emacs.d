33f4d93e138d61c7e62e913faf306d36a0a510e7
Marcos Moritz <marcosmoritz@gmail.com>
2016-12-23 02:37:46 -0200

    Add some customization for org source blocks

--- prettify-symbols.el
+++ prettify-symbols.el
@@ -1,5 +1,9 @@
+;;; prettify-symbols --- Pretty symbols substitution configuration
+;;; Commentary:
+
+;;; Code:
 (defun js2-pretty-symbols ()
-  "make some word or string show as pretty Unicode symbols for js2-mode"
+  "Make some word or string show as pretty Unicode symbols for \"js2-mode\"."
   (setq prettify-symbols-alist
         '(
           ("lambda" . 955)   ; λ
@@ -10,7 +14,7 @@
   (prettify-symbols-mode))
 
 (defun python-pretty-symbols ()
-  "make some word or string show as pretty Unicode symbols for python-mode"
+  "Make some word or string show as pretty Unicode symbols for \"python-mode\"."
   (setq prettify-symbols-alist
         '(
           ("lambda" . 955)   ; λ
@@ -20,15 +24,30 @@
 
 
 (defun emacs-lisp-pretty-symbols ()
-  "make some word or string show as pretty Unicode symbols for emacs-lisp-mode"
+  "Make some word or string show as pretty Unicode symbols for \"emacs-lisp-mode\"."
   (setq prettify-symbols-alist
         '(
           ("lambda" . 955)   ; λ
           ))
   (prettify-symbols-mode))
 
+(defun org-pretty-symbols ()
+  "Make some word or string show as pretty Unicode symbols for \"org-mode\"."
+  (setq prettify-symbols-alist
+        '(
+          ("#+BEGIN_SRC" . 160)   ; nbsp
+          ("#+begin_src" . 160)   ; nbsp
+          ("#+END_SRC" . 160)   ; nbsp
+          ("#+end_src" . 160)   ; nbsp
+          ))
+  (prettify-symbols-mode))
+
+
 (add-hook 'js2-mode-hook 'js2-pretty-symbols)
 (add-hook 'python-mode-hook 'python-pretty-symbols)
 (add-hook 'emacs-lisp-mode-hook 'emacs-lisp-pretty-symbols)
+(add-hook 'org-mode-hook 'org-pretty-symbols)
 
+
+;;; prettify-symbols.el ends here
 
